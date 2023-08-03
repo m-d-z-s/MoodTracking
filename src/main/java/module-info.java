3a8cab4d83f61208b795daf4e81b1fd5d1588b78
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-08-03 16:57:55 +0300

    initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module me.mdzs.moodtracking {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens me.mdzs.moodtracking to javafx.fxml;
+    exports me.mdzs.moodtracking;
+    exports me.mdzs.moodtracking.domain;
+    opens me.mdzs.moodtracking.domain to javafx.fxml;
+}
